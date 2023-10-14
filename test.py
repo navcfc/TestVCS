ef04d1bb6dc6dfb75b5d8c3de2c0c977d4607192
navcfc <navinash03@gmail.com>
2023-10-15 01:32:33 +0400

    added new test

--- test.py
+++ test.py
@@ -3,11 +3,11 @@ a = 10
 b = 5
 c = a + b
 print(c)
-
+d = a - b
 # add the two numnbers together
 a = 10 + c
 print(a)
-
+print(d)
 # add 19090 and a to print and add to a list
 a = 19090 + a
 print(a)
